4cdbc02f593bccbe88d8135fe04879c5e1d494fe
aryanpatial74-ctrl <aryanpatial74@gmail.com>
2026-04-22 15:59:29 +0530

    Add files via upload

---
+++ Libray.py
@@ -0,0 +1,77 @@
+books = [
+    "The Midnight Library",
+    "Sapiens: A Brief History of Humankind",
+    "Educated",
+    "The Silent Patient",
+    "Atomic Habits",
+    "Norwegian Wood",
+    "The Alchemist",
+    "Project Hail Mary"
+]
+issued_books = ["The Midnight Library",
+    "Sapiens: A Brief History of Humankind",
+    "Educated"]
+
+
+def add_book():
+    name = input("Enter book name : ")
+    books.append(name)
+    print(name, "added")
+
+
+def show_book():
+    if len(books) == 0:
+        print("No books Avaliable")
+    else:
+        print(books)
+    for i in books:
+        print(i)
+
+
+def issue_book():
+    if len(books) == 0:
+        print("No books avaliable")
+    else:
+        show_book()
+        name = input("Enter the book anme :")
+        if name in books:
+            books.remove(name)
+            issued_books.append(name)
+            print(name, "book issued")
+
+
+def return_book():
+    name = input("Enter book name you want to return :")
+    if name is issued_books:
+        issued_books.remove(name)
+        books.append(name)
+        print(name, "Book returned")
+    else:
+        print(name, "book was not issued")
+
+
+def library():
+    while True:
+        print("\n----menu----")
+        print("1.Add Book")
+        print("2.Show Books")
+        print("3 Issue Book")
+        print("4.Return Book")
+        print("5.Exit")
+        choice = int(input("\nEnter your choice : "))
+        if choice == 1:
+            add_book()
+        elif choice == 2:
+            show_book()
+        elif choice == 3:
+            issue_book()
+        elif choice == 4:
+            return_book()
+        elif choice == 5:
+            print("Thank You!")
+            break
+        else:
+            print("Invalid choice")
+
+
+library()
